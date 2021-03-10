51486a71fd438508bbaefa27c60dda4f7acadee8
DexSinis (from Travis CI) <343731621@qq.com>
2021-03-10 09:55:29 +0000

    Deploy <custom-domain-if-needed> to github.com/DexSinis/Dexsinis.github.io.git:master

--- en.search.min.67528c087f99fc8ce0eaaff5b76eeff676173fc21bbf40dcaeea9ff87b85966b.js
+++ en.search.min.67528c087f99fc8ce0eaaff5b76eeff676173fc21bbf40dcaeea9ff87b85966b.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.5ebb00b7a86d986d03982d3d4b0ca9d5453d070b03932968f1a35a1d84526147.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.ec7ef3f650b5993ad04dd3ee6372a4dbe4b37d8f9677278f4a2a5286663cb6b6.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
